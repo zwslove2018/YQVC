4064a5397764784c87e69067cdd811c8ddc0e6e9
zwslove2018 <zwslove888@163.com>
2018-07-16 14:37:01 +0800

    new create

---
+++ YQVC/Ext/CorButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace YQVC.Ext
+{
+    public class CorButton : Button
+    {
+        static CorButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CorButton), new FrameworkPropertyMetadata(typeof(CorButton)));
+        }
+        public static readonly DependencyProperty CorProperty = DependencyProperty.Register("Cor", typeof(CornerRadius), typeof(CorButton));
+        public static readonly DependencyProperty HoverProperty = DependencyProperty.Register("Hover", typeof(Brush), typeof(CorButton));
+        public CornerRadius Cor
+        {
+            get { return (CornerRadius)GetValue(CorProperty); }
+            set { SetValue(CorProperty, value); }
+        }
+        public Brush Hover
+        {
+            get { return (Brush)GetValue(HoverProperty); }
+            set { SetValue(HoverProperty, value); }
+        }
+    }
+}
---
+++ YQVC/Ext/WaterMarkBox.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace YQVC.Ext
+{
+    public class WaterMarkBox : TextBox
+    {
+        static WaterMarkBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(WaterMarkBox), new FrameworkPropertyMetadata(typeof(WaterMarkBox)));
+        }
+        public static readonly DependencyProperty MaskProperty = DependencyProperty.Register("Mask", typeof(string), typeof(WaterMarkBox), new PropertyMetadata("请输入文本"));
+        public string Mask
+        {
+            get { return (string)GetValue(MaskProperty); }
+            set { SetValue(MaskProperty, value); }
+        }
+    }
+}
